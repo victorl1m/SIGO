065bb417648d246fcbae39bedd2943cab0e418aa
Victor Lima <trackedby1@gmail.com>
2022-11-05 00:37:18 -0300

    SIG-21 forgot screen created from scratch :)

--- src/screens/ForgotPW.jsx
+++ src/screens/ForgotPW.jsx
@@ -1,29 +1,91 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+    Pressable,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    Alert,
+    SafeAreaView,
+  } from "react-native";
 
 export function ForgotPW({ navigation }) {
+  function goBack() {
+    const { navigate } = navigation;
+    navigate("Login");
+  }
 
-    function goBack() {
-        const { navigate } = navigation;
-        navigate('Login');
-    }
-
-    return (
-        <View style={styles.container}>
-            <Text>Do you Forgot? :(</Text>
-            <Button 
-                title="go back"
-                onPress={goBack}
-            />
-        </View>
-    )
+  return (
+    <SafeAreaView style={styles.forgotContainer}>
+      <View style={styles.forgotArea}>
+        <Text style={styles.forgotTitle}>Esqueci minha senha</Text>
+        <Text style={styles.forgotSubtitle}>
+          Preencha o campo abaixo com seu e-mail {"\n"} para recuperar a sua senha
+        </Text>
+        <TextInput
+          style={styles.forgotInput}
+          placeholder="Email"
+          placeholderTextColor="#AAAAAA"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        ></TextInput>
+        <Pressable style={styles.forgotBtn}>
+          <Text style={styles.buttonText}>Recuperar acesso</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: 10
-    }
+  registerContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#121212",
+  },
+  forgotArea: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#121212",
+    width: "100%",
+    height: "100%",
+  },
+  forgotTitle: {
+    fontFamily: "Montserrat-Bold",
+    fontSize: 30,
+    color: "#00B2CB",
+  },
+  forgotSubtitle: {
+    fontFamily: "Montserrat-Medium",
+    fontSize: 15,
+    color: "white",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  forgotInput: {
+    backgroundColor: "#1E1E1E",
+    width: "90%",
+    height: 70,
+    borderRadius: 10,
+    padding: 12,
+    marginBottom: 10,
+    fontSize: 16,
+    fontFamily: "Montserrat-Medium",
+    color: "#00B2CB",
+  },
+  forgotBtn: {
+    backgroundColor: "#00B2CB",
+    width: "90%",
+    height: 80,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 12,
+  },
+  buttonText: {
+    color: "white",
+    fontFamily: "Montserrat-Bold",
+    fontSize: 24,
+  },
 });
-
