e52869e2d637b234ef8cdd103d0f00a097c568a7
vsrg <vsr@tuta.io>
2022-12-20 19:14:15 -0300

    adding database tables

--- src/sigo_js/_database.js
+++ src/sigo_js/_database.js
@@ -1,11 +1,17 @@
+const db_user = 'postgres'
+const db_host = 'localhost'
+const db_name = 'postgres'
+const db_password = 'sigo'
+const db_port = '5432'
+
 const pg = require('pg')
 
 const client = new pg.Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'sigo',
-  port: 5432,
+  user: db_user,
+  host: db_host,
+  database: db_name,
+  password: db_password,
+  port: db_port,
 })
 
 module.exports = client
--- src/sigo_js/createTables.js
+++ src/sigo_js/createTables.js
@@ -4,21 +4,32 @@ async function createTables(){
   await db.connect()
 
   await db.query(`CREATE TABLE tb_client(
-   id serial PRIMARY KEY,
-   client_name VARCHAR (50) UNIQUE NOT NULL
+   client_id serial PRIMARY KEY,
+   client_name VARCHAR (50) UNIQUE NOT NULL,
+   client_zipcode VARCHAR (10),
+   client_street VARCHAR (50),
+   client_number NUMERIC
   )`)
 
-  await db.query(`CREATE TABLE tb_jobsite(
-   id serial PRIMARY KEY,
-   jobsite_name VARCHAR (50) UNIQUE NOT NULL
+  await db.query(`CREATE TABLE tb_exec_tasks(
+    jobsite_id integer NOT NULL PRIMARY KEY,
+    type VARCHAR (50),
+    category VARCHAR (50),
+    subcategory VARCHAR (50),
+    status VARCHAR (50)
   )`)
 
-  await db.query(`CREATE TABLE tb_client_jobsite(
-    client_id integer NOT NULL,
-    jobsite_id integer NOT NULL,
-    PRIMARY KEY (client_id, jobsite_id),
-    FOREIGN KEY (client_id) REFERENCES tb_client (id),
-    FOREIGN KEY (jobsite_id) REFERENCES tb_jobsite (id)
+  await db.query(`CREATE TABLE tb_admin_tasks(
+    jobsite_id integer NOT NULL PRIMARY KEY,
+    category VARCHAR (50),
+    subcategory VARCHAR (50),
+    assignee VARCHAR (50),
+    priority VARCHAR (50),
+    status VARCHAR (50),
+    budgeted MONEY,
+    expended MONEY,
+    deadline DATE,
+    notes VARCHAR (200)
   )`)
 
   await db.end()
